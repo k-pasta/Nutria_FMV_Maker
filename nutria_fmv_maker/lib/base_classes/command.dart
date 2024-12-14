8c70e80975ada2db0d92fc98c520e1838f4acb75
ksepastremenos <k-pasta@mail.com>
2024-12-14 18:20:51 +0200

    Updated Graph, Will update node data system

--- nutria_fmv_maker/lib/base_classes/command.dart
+++ nutria_fmv_maker/lib/base_classes/command.dart
@@ -1,4 +1,6 @@
 abstract class Command {
+///Interface for creating undo/redo patterns
+///implement this to any function needs to be put in the undo & redo stacks.
   void execute();
   void undo();
 }
--- nutria_fmv_maker/lib/providers/nodes_provider.dart
+++ nutria_fmv_maker/lib/providers/nodes_provider.dart
@@ -61,30 +61,6 @@ class NodesProvider extends ChangeNotifier {
     notifyListeners();
   }
 
-// void updateNodePosition(String id, Offset newPosition) {
-//   // Find the index of the node by its id
-//   final nodeIndex = _nodes.indexWhere((n) => n.id == id);
-
-//   if (nodeIndex == -1) {
-//     throw Exception("Node not found");
-//   }
-
-//   // Update the node's position
-//   final node = _nodes[nodeIndex];
-//   node.position = newPosition;
-
-//   // If the node is not already at the bottom of the list, move it
-//   if (nodeIndex != _nodes.length - 1) {
-
-//     // Remove the node and add it to the bottom of the list
-//     _nodes.removeAt(nodeIndex);
-//     _nodes.add(node);
-//   }
-//     print(nodes[0].id + nodes[1].id + nodes[2].id);
-//   // Notify listeners after making changes
-//   notifyListeners();
-// }
-
   void onNoodlesUpdate() {}
 
   void updateNodePosition(String id, Offset newPosition) {
